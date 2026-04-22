9bad6f1a8fc93f1f3a1e885aa8b89c5e9ef0515f
Marco Kretz <marco@codebites.de>
2026-04-22 17:15:18 +0200

    chore: removed legacy config stuff

--- old/dot_config/hypr/scripts/executable_start-keyring.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if gnome-keyring-daemon is already running and start it if not
-if ! pgrep -x gnome-keyring-daemon > /dev/null; then
-    eval "$(gnome-keyring-daemon --start)"
-fi
-
